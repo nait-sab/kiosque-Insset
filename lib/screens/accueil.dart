285e4b960a0fdef0b9f4a3f4869f596a45b9f459
nait-sab <naitsab.boulogne@gmail.com>
2021-09-20 11:10:17 +0200

    Change name and starter

---
+++ lib/screens/accueil.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Accueil extends StatelessWidget {
+  const Accueil({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.lightBlue,
+      ),
+    );
+  }
+}
